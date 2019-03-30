27326d29b4f8c0542007be857510a53f1b524152
Rob Bowden <rbowden91@gmail.com>
2019-03-30 06:28:31 -0400

    fix tg

--- src/repair50/server/server.py
+++ src/repair50/server/server.py
@@ -194,12 +194,12 @@ class Server(object):
                                                     token[1], revlex['attr'][str(j)]) for j in range(len(attr_probs))])
                             probs = new_probs
                             p['actual_attr'] = revlex['attr'][str(attr_target)]
-                            p['actual_label'] = revlex[key][str(token_target)]
+                            p['actual_label'] = revlex['label'][str(token_target)]
                         else:
                             probs = [(float(probs[j]), revlex['transitions'][str(j)]) for j in range(len(probs))]
                             #probs = probs.tolist()
                             #print(config['transitions_groups'])
-                            p['actual_transitions'] = revlex[key][str(token_target)]
+                            p['actual_transitions'] = revlex['transitions'][str(token_target)]
                             p['transitions_groups'] = config['transitions_groups'][p['actual_transitions']] if p['actual_transitions'] in config['transitions_groups'] else False
 
                         probs.sort(key=lambda x: x[0], reverse=True)
