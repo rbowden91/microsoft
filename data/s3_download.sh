9fc45965c29f6873af799086ed3c677690a62b58
Rob Bowden <rbowden91@gmail.com>
2019-05-22 04:20:02 -0400

    update  s3_download

--- data/s3_download.sh
+++ data/s3_download.sh
@@ -6,15 +6,19 @@ download () {
     cd $num
     #https://github.com/andrewrk/node-s3-cli
     #sudo npm install -g s3-cli
-    s3-cli ls s3://sandbox50x/uploads/ .
+    while IFS= read -r line; do
+	line=$(echo "$line" | cut -b '13-')
+	echo "$line"
+	s3-cli sync s3://sandbox50x/uploads/$line "$line" 2> /dev/null
+    done < <(s3-cli ls s3://sandbox50x/uploads/$num)
 }
 
 mkdir -p cs50_data/s3_unsorted_downloads
 cd cs50_data/s3_unsorted_downloads
-for i in $(seq -f "%g" 00000 65535); do
+for i in $(seq -f "%g" 50000 65535); do
     num=`printf "%04x" $i`
     children=$(($(pgrep --parent $$ | wc -l) - 1))
-    while (( children > 1000 )); do
+    while (( children > 100 )); do
     	sleep 1
 	children=$(($(pgrep --parent $$ | wc -l) - 1))
     done
